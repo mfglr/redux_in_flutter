43ae88ae3ea8957a271ebb0cf6f77020e4ef0a6d
mfglr <mfgglr@outlook.com>
2024-07-08 12:31:47 +0300

    Created AddItemAction.

--- lib/action.dart
+++ lib/action.dart
@@ -9,6 +9,12 @@ abstract class Action{
 @immutable
 class ChangeFilterTypeAction extends Action{
   final ItemFilter filter;
-
   const ChangeFilterTypeAction({required this.filter});
 }
+
+@immutable
+class AddItemAction{
+  final String item;
+  const AddItemAction({required this.item});
+}
+
