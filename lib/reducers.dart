2a1d11a534276b71d8625e6699ebab95b7404eb7
mfglr <mfgglr@outlook.com>
2024-07-08 13:00:07 +0300

    Created RemoveItemReducer

--- lib/reducers.dart
+++ lib/reducers.dart
@@ -2,3 +2,6 @@ import 'package:redux_in_flutter/actions.dart';
 
 Iterable<String> addItemReducer(Iterable<String> prevItems,AddItemAction action)
   => prevItems.followedBy([action.item]);
+
+Iterable<String> removeItemReducer(Iterable<String> prevItems,RemoveItemAction action)
+  => prevItems.where((item) => item != action.item);
