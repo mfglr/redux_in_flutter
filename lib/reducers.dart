d33a2403532e5b6a06367c944bbe1ed3f897bcae
mfglr <mfgglr@outlook.com>
2024-07-08 13:49:55 +0300

    Created appStateReducer.

--- lib/reducers.dart
+++ lib/reducers.dart
@@ -1,6 +1,7 @@
 import 'package:redux_in_flutter/actions.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_in_flutter/item_filter.dart';
+import 'package:redux_in_flutter/app_state.dart';
 
 Iterable<String> addItemReducer(Iterable<String> prevItems,AddItemAction action)
   => prevItems.followedBy([action.item]);
@@ -13,9 +14,15 @@ Reducer<Iterable<String>> itemReducer = combineReducers<Iterable<String>>([
   TypedReducer<Iterable<String>,RemoveItemAction>(removeItemReducer).call,
 ]);
 
+
 ItemFilter itemFilterReducer(ItemFilter oldItemFilter,Action action){
   if(action is ChangeFilterTypeAction) action.itemFilter;
   return oldItemFilter;
 }
 
 
+AppState appStateReducer(AppState oldState,Action action) 
+  => AppState(
+    items: itemReducer(oldState.items,action),
+    itemFilter: itemFilterReducer(oldState.itemFilter, action)
+  );
